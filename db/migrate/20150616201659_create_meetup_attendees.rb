3f4ea3c993aaa0e8a6a9afde34184b30fc8d1884
kat3185 <Ken.Thomas.3185@gmail.com>
2015-06-16 16:27:15 -0400

    added meetup_attendees table - finish day 1

---
+++ db/migrate/20150616201659_create_meetup_attendees.rb
@@ -0,0 +1,8 @@
+class CreateMeetupAttendees < ActiveRecord::Migration
+  def change
+    create_table :meetup_attendees do |t|
+      t.integer :user_id, null: false
+      t.integer :meetup_id, null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150616181837) do
+ActiveRecord::Schema.define(version: 20150616201659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "meetup_attendees", force: true do |t|
+    t.integer "user_id",   null: false
+    t.integer "meetup_id", null: false
+  end
 
   create_table "meetup_owners", force: true do |t|
     t.integer "user_id",   null: false
